3f487bb5fe9d6eaa2410c3fc4683026405149970
Judith18 <judicriss18@gmail.com>
2019-02-26 13:06:48 -0500

    Cifrado y Descifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,49 +1,51 @@
 //****************************FUNCION CIFRAR*******************************
 window.cipher = {
-  encode: (offset, string) => {
-  let letterToAsciiNum;
-  let resultOfEncode = "";
-//RECORRIDO EL MENSAJE DEL USUARIO
-		for (let i = 0; i < string.length; i++){
-		letterToAsciiNum = string.charCodeAt(i);
-//Asimila mayúsculas
-		if (letterToAsciiNum >= 65 && letterToAsciiNum <= 90){
-			letterToAsciiNum = String.fromCharCode((letterToAsciiNum - 65 + parseInt(offset)) % 26 + 65);
-			resultOfEncode += letterToAsciiNum;
-//Asimila minisculas
-		} else if (letterToAsciiNum >= 97 && letterToAsciiNum <= 122){
-			letterToAsciiNum = String.fromCharCode((letterToAsciiNum - 97 +  parseInt(offset)) % 26 + 97);
-			resultOfEncode +=  letterToAsciiNum;
-//Asimila el espacio " "
-		} else if (letterToAsciiNum === 32){
-      letterToAsciiNum = String.fromCharCode(letterToAsciiNum);
-			resultOfEncode += letterToAsciiNum;
+	encode: (offset, string) => {
+		let letterToAsciiNum;
+		let resultOfEncode = "";
+		//RECORRIDO EL MENSAJE DEL USUARIO
+		//debugger
+		for (let i = 0; i < string.length; i++) {
+			letterToAsciiNum = string.charCodeAt(i);
+			//Asimila mayúsculas
+			if (letterToAsciiNum >= 65 && letterToAsciiNum <= 90) {
+				letterToAsciiNum = String.fromCharCode((letterToAsciiNum - 65 + parseInt(offset)) % 26 + 65);
+				resultOfEncode += letterToAsciiNum;
+				//Asimila minisculas
+			} else if (letterToAsciiNum >= 97 && letterToAsciiNum <= 122) {
+				letterToAsciiNum = String.fromCharCode((letterToAsciiNum - 97 + parseInt(offset)) % 26 + 97);
+				resultOfEncode += letterToAsciiNum;
+				//Asimila el espacio " "
+			} else if (letterToAsciiNum === 32) {
+				letterToAsciiNum = String.fromCharCode(letterToAsciiNum);
+				resultOfEncode += " ";
+			}
 		}
-    }
-	console.log(resultOfEncode);
-} ,
-  //****************************FUNCION DESCIFRAR*******************************
+		//resultOfEncode = "holi"
+		return resultOfEncode;
+	},
+	//****************************FUNCION DESCIFRAR*******************************
 
-  decode : (offset, string) => {
-	let letterToAsciiNum;
-  let resultOfDecode = "";
-	let key = parseInt(offset) % 26;
-//RECORRIDO EL MENSAJE DEL USUARIO
-	for (let i = 0; i < string.length; i++){
-		letterToAsciiNum = string.charCodeAt(i);
-//Asimila mayúsculas
-		if (letterToAsciiNum >= 65 && letterToAsciiNum <= 90){
-			letterToAsciiNum = String.fromCharCode ((letterToAsciiNum + 65 - key) % 26 + 65);
-			resultOfDecode += letterToAsciiNum;
-//Asimila minisculas
-		} else if (letterToAsciiNum >= 97 && letterToAsciiNum <= 122){
-			letterToAsciiNum = String.fromCharCode((letterToAsciiNum + 85 - key) % 26 + 97);
-			resultOfDecode += letterToAsciiNum;
-//Asimila el espacio " "
-		} else if (letterToAsciiNum === 32){
-			resultOfDecode += " ";
+	decode: (offset, string) => {
+		let letterToAsciiNum;
+		let resultOfDecode = "";
+		let key = parseInt(offset) % 26;
+		//RECORRIDO EL MENSAJE DEL USUARIO
+		for (let i = 0; i < string.length; i++) {
+			letterToAsciiNum = string.charCodeAt(i);
+			//Asimila mayúsculas
+			if (letterToAsciiNum >= 65 && letterToAsciiNum <= 90) {
+				letterToAsciiNum = String.fromCharCode((letterToAsciiNum + 65 - key) % 26 + 65);
+				resultOfDecode += letterToAsciiNum;
+				//Asimila minisculas
+			} else if (letterToAsciiNum >= 97 && letterToAsciiNum <= 122) {
+				letterToAsciiNum = String.fromCharCode((letterToAsciiNum + 111 - key) % 26 + 97);
+				resultOfDecode += letterToAsciiNum;
+				//Asimila el espacio " "
+			} else if (letterToAsciiNum === 32) {
+				resultOfDecode += " ";
+			}
 		}
+		return resultOfDecode;
 	}
-	console.log(resultOfDecode);
-}
 };
--- src/index.js
+++ src/index.js
@@ -9,16 +9,23 @@ function validarNombre() {
 }
 
 
-let string = document.getElementById('string');
-let offset = document.getElementById('offset');
+let string;
+let offset;
 let cifrarButton = document.getElementById('encode-button');
 let descifrarButton = document.getElementById('decode-button');
 let newResultCipher;
+let newResultDecipher
 //**********************EVENTO DEL BOTON CIFRAR**************************
 cifrarButton.addEventListener("click", ()  => {
 	string = document.getElementById('string').value;
-    offset = parseInt(document.getElementById('offset')).value;
+    offset = parseInt(document.getElementById('offset').value);
     newResultCipher= cipher.encode(offset, string);
     document.getElementById("string1").value = newResultCipher;
     });
     
+descifrarButton.addEventListener("click", ()  => {
+        string = document.getElementById('string').value;
+        offset = parseInt(document.getElementById('offset').value);
+        newResultDecipher= cipher.decode(offset, string);
+        document.getElementById("string1").value = newResultDecipher;
+        });
