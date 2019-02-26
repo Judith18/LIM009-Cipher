f14e72dd9d06d25d2de8bc592519d92f153c38d5
Judith18 <judicriss18@gmail.com>
2019-02-26 06:39:53 -0500

    Añadiendo carpeta de Imagenes,cipher.js,index.js,index.html y style.css

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,49 @@
+//****************************FUNCION CIFRAR*******************************
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
+  encode: (offset, string) => {
+  let letterToAsciiNum;
+  let resultOfEncode = "";
+//RECORRIDO EL MENSAJE DEL USUARIO
+		for (let i = 0; i < string.length; i++){
+		letterToAsciiNum = string.charCodeAt(i);
+//Asimila mayúsculas
+		if (letterToAsciiNum >= 65 && letterToAsciiNum <= 90){
+			letterToAsciiNum = String.fromCharCode((letterToAsciiNum - 65 + parseInt(offset)) % 26 + 65);
+			resultOfEncode += letterToAsciiNum;
+//Asimila minisculas
+		} else if (letterToAsciiNum >= 97 && letterToAsciiNum <= 122){
+			letterToAsciiNum = String.fromCharCode((letterToAsciiNum - 97 +  parseInt(offset)) % 26 + 97);
+			resultOfEncode +=  letterToAsciiNum;
+//Asimila el espacio " "
+		} else if (letterToAsciiNum === 32){
+      letterToAsciiNum = String.fromCharCode(letterToAsciiNum);
+			resultOfEncode += letterToAsciiNum;
+		}
+    }
+	console.log(resultOfEncode);
+} ,
+  //****************************FUNCION DESCIFRAR*******************************
+
+  decode : (offset, string) => {
+	let letterToAsciiNum;
+  let resultOfDecode = "";
+	let key = parseInt(offset) % 26;
+//RECORRIDO EL MENSAJE DEL USUARIO
+	for (let i = 0; i < string.length; i++){
+		letterToAsciiNum = string.charCodeAt(i);
+//Asimila mayúsculas
+		if (letterToAsciiNum >= 65 && letterToAsciiNum <= 90){
+			letterToAsciiNum = String.fromCharCode ((letterToAsciiNum + 65 - key) % 26 + 65);
+			resultOfDecode += letterToAsciiNum;
+//Asimila minisculas
+		} else if (letterToAsciiNum >= 97 && letterToAsciiNum <= 122){
+			letterToAsciiNum = String.fromCharCode((letterToAsciiNum + 85 - key) % 26 + 97);
+			resultOfDecode += letterToAsciiNum;
+//Asimila el espacio " "
+		} else if (letterToAsciiNum === 32){
+			resultOfDecode += " ";
+		}
+	}
+	console.log(resultOfDecode);
+}
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-/* Acá va tu código */
+document.getElementById("Pagina2").style.display = "none";
+
+function validarNombre() {
+    const name2= document.getElementById("txt").value;
+    console.log(name2);
+    document.getElementById("nombreUsuario").innerHTML = name2;
+    document.getElementById("Pagina1").style.display = "none";
+    document.getElementById("Pagina2").style.display = "block";
+}
+
+
+let string = document.getElementById('string');
+let offset = document.getElementById('offset');
+let cifrarButton = document.getElementById('encode-button');
+let descifrarButton = document.getElementById('decode-button');
+let newResultCipher;
+//**********************EVENTO DEL BOTON CIFRAR**************************
+cifrarButton.addEventListener("click", ()  => {
+	string = document.getElementById('string').value;
+    offset = parseInt(document.getElementById('offset')).value;
+    newResultCipher= cipher.encode(offset, string);
+    document.getElementById("string1").value = newResultCipher;
+    });
+    
